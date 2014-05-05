2ecea4d81e08a6aff3b38ea7066c79790021b4e5
cww0909 <cww9989@gmail.com>
2014-05-05 00:30:20 -0700

    fixed manifest permissions

--- scripts/pingpingping.js
+++ scripts/pingpingping.js
@@ -2,7 +2,7 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
     dataRange = 1000,
-    interval = 1000;
+    interval = 2000;
 
 //TODO: look into d3.time.forat.multi x axis tick labels
 
